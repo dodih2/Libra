03f159f427df103f1bf247b06745cee148cf6f5f
ismatul12345 <34561501+ismatul12345@users.noreply.github.com>
2018-01-07 16:54:40 +0700

    penambahan pesan by ismatul

---
+++ kirimpesan/setting/database.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "libra_db";
+$koneksi = mysqli_connect($server,$username,$password, $database) or die("Koneksi gagal");
+?>
