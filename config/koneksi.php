024d89748323971af9e0d99331520c8ef463820b
ismatul12345 <34561501+ismatul12345@users.noreply.github.com>
2018-01-07 16:51:34 +0700

    koneksi db dan class pagging

---
+++ config/koneksi.php
@@ -0,0 +1,7 @@
+<?php
+$server = "localhost";
+$username = "root";
+$password = "";
+$database = "libra_db";
+$conn = mysqli_connect($server,$username,$password, $database) or die("Koneksi gagal");
+?>
